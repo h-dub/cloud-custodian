081f8fc00edf3c23aa74a8de78211424165ee11d
h-dub <72996437+h-dub@users.noreply.github.com>
2020-11-13 14:48:05 -0800

    Fix test race condition
    The delete is not always committed by the time the test checks for it. When the test is running fast enough, it fails.

--- tools/c7n_azure/tests_azure/tests_resources/test_event_subscriptions.py
+++ tools/c7n_azure/tests_azure/tests_resources/test_event_subscriptions.py
@@ -76,5 +76,6 @@ class AzureEventSubscriptionsTest(BaseTest):
         }, validate=True)
 
         p_delete.run()
+        self.sleep_in_live_mode(5)
         resources_post_delete = p_get.run()
         self.assertEqual(len(resources_post_delete), 0)
